e636c59acc5a24fbab01e102d70b122fd2e8b447
duvaudierje <jerome.duvaudier@natixis.com>
2021-04-23 17:29:01 +0200

    Soluce quete Spring 01

---
+++ src/main/java/com/wildcodeschool/mySuperProject/MySuperProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.mySuperProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySuperProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySuperProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/mySuperProject/MySuperProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.mySuperProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySuperProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
